29cf40193be1484371426af4c6d9b6d453ba4f87
Aristofanis Chionis <aristofanischionis@gmail.com>
2017-11-25 16:23:24 +0000

    installing pg

--- index2.js
+++ index2.js
@@ -7,7 +7,7 @@ var bodyParser = require("body-parser");
 // var facebook = require("./myfacebook");
 // var postgres = require("postgres");
 const { Client } = require('pg');
-
+const connectionString = process.env.DATABASE_URL;
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
@@ -27,6 +27,35 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
    res.send("rigrgrigrg");
+});
+
+app.post("/bot", function(req, res){
+    const results = [];
+    // Grab data from http request
+  const data = {text: req.body.text, complete: false};
+  // Get a Postgres client from the connection pool
+  client.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Insert Data
+    client.query('INSERT INTO items(text, complete) values($1, $2)',
+    [data.text, data.complete]);
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM items ORDER BY id ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
 });
 
 app.listen(process.env.PORT,process.env.IP,function(){
