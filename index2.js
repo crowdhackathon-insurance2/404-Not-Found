e46819c63ec90d601b773ddbf5cad5820a45c96c
Aristofanis Chionis <aristofanischionis@gmail.com>
2017-11-25 18:16:48 +0000

    test database

--- index2.js
+++ index2.js
@@ -26,80 +26,26 @@ function onConnect(err,client,done){
     client.end();
 }
 
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true,
-// });
 
-// client.connect();
+function getUser(req,res,next){
+    var userID = parseInt(req.params.id);
+    app.one('select * from users where id = $1',userID)
+        .then(function(data){
+            res.status(200)
+            .json({
+                status:'success',
+                data:data,
+                message: 'Retrieved user'
+            });
+        })
+        .catch(function(err){
+            return next(err);
+        });
+}
 
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
-
-// app.use(bodyParser.urlencoded({extended: true}));
-
-// app.get("/", function(req, res){
-//   res.send("rigrgrigrg");
-// });
-
-// app.post("/bot", function(req, res){
-//     const results = [];
-//     // Grab data from http request
-//   const data = {text: req.body.text, complete: false};
-//   // Get a Postgres client from the connection pool
-//   client.connect(connectionString, (err, client, done) => {
-//     // Handle connection errors
-//     if(err) {
-//       done();
-//       console.log(err);
-//       return res.status(500).json({success: false, data: err});
-//     }
-//     // SQL Query > Insert Data
-//     client.query('INSERT INTO items(text, complete) values($1, $2)',
-//     [data.text, data.complete]);
-//     // SQL Query > Select Data
-//     const query = client.query('SELECT * FROM items ORDER BY id ASC');
-//     // Stream results back one row at a time
-//     query.on('row', (row) => {
-//       results.push(row);
-//     });
-//     // After all data is returned, close connection and return results
-//     query.on('end', () => {
-//       done();
-//       return res.json(results);
-//     });
-//   });
-// });
-
-// app.get("/bot", function(req, res){
-//     const results = [];
-//   // Get a Postgres client from the connection pool
-//   const data = {}
-//   client.connect(connectionString, (err, client, done) => {
-//     // Handle connection errors
-//     if(err) {
-//       done();
-//       console.log(err);
-//       return res.status(500).json({success: false, data: err});
-//     }
-//     // SQL Query > Select Data
-//     const query = client.query('SELECT * FROM items ORDER BY id ASC;');
-//     // Stream results back one row at a time
-//     query.on('row', (row) => {
-//       results.push(row);
-//     });
-//     // After all data is returned, close connection and return results
-//     query.on('end', () => {
-//       done();
-//       return res.json(results);
-//     });
-//   });
-// });
+app.get("/",function(req,res){
+    
+})
 
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("server started");
